aac9cddd38bc1eb43ecaf99fa16c66642838068f
Felipe GM <bebefofo123abc@gmail.com>
2020-03-03 20:18:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,6 @@ function createWindow () {
 
   win.loadFile("./src/index.html");
 
-
   win.on("closed", () => {win = null});
   win.setIgnoreMouseEvents(true)
   win.setAlwaysOnTop(true, "floating", 1);
@@ -29,7 +28,7 @@ function createWindow () {
 
     if(input.control === true) {
       let mousePos = screen.getCursorScreenPoint();
-      win.setPosition(mousePos.x, mousePos.y)
+      win.setPosition(mousePos.x, mousePos.y);
     }
 
     if(location !== "bot") {
@@ -38,13 +37,12 @@ function createWindow () {
     
     if(input.key === "b" || input.key === "n" || input.key === "v" || input.key === "c" || input.key === "f") {
       if(input.type === "keyDown") {
-      win.setIgnoreMouseEvents(false)
+        win.setIgnoreMouseEvents(false)
       } else { 
         win.setIgnoreMouseEvents(true);
       }
     }
-
-  })
+  });
 }
 
 app.on("ready", createWindow);
@@ -58,13 +56,16 @@ app.on("activate", () => {
 ipcMain.on("synchronous-message", (event, arg) => {
 
   if(typeof arg === "string") {
-    if(arg == "mouseOn") win.setIgnoreMouseEvents(false);
-    if(arg == "mouseOff") win.setIgnoreMouseEvents(true);
+    if(arg === "mouseOn") {
+      win.setIgnoreMouseEvents(false);
+    } else if(arg === "mouseOff") {
+      win.setIgnoreMouseEvents(true);
+    }
   }
 
-  else if(typeof arg == "object") {
+  else if(typeof arg === "object") {
     if(arg.info === "reSize") {
-      win.setSize(arg.width,arg.height)
+      win.setSize(arg.width, arg.height);
       location = arg.location;
     } else if(arg.info === "ad") {
       shell.openExternal(arg.link);
