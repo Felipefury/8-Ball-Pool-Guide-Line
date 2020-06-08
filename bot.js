349a7b6f037afa2275483e34733c1499b4f86e16
ZeedyDF <marcosdanttas18@hotmail.com>
2020-06-08 01:27:01 -0300

    v3.1 - type fixed

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, screen, globalShortcut,ipcMain} = require("electron"
 const shell = require("electron").shell;
 
 let win;
-let location = "bot"
+let location = "login"
 let winxy = {x:0,y:0}
 
 function createWindow () {
@@ -50,7 +50,24 @@ app.whenReady().then(() => {
   console.log(' _/  _/    _/  _/    _/    ');
   console.log('_/    _/_/_/  _/_/_/       \n\n');
 
-  console.log('(You can\'t type anymore while using the hack)\n\nV3 coded By Zeedy and GM\nIf you have any suggestion message us on discord.\n\nDon\'t close the CMD while using the hack.');
+  console.log('V3.1 coded By Zeedy and GM\nIf you have any suggestion message us on discord.\n\nDon\'t close the CMD while using the hack.');
+})
+
+function registerX() {
+
+  globalShortcut.register('Shift+X', () => {
+    if(win.getOpacity() <= 0) {
+      win.setOpacity(1);
+      register();
+    } else { 
+      win.setOpacity(0);
+      globalShortcut.unregisterAll()
+      setTimeout(registerX, 500);
+    }
+  });
+}
+
+function register() {
   globalShortcut.register('Z', () => {
     let mousePos = screen.getCursorScreenPoint();
     win.setPosition(mousePos.x, mousePos.y);
@@ -58,45 +75,43 @@ app.whenReady().then(() => {
     winxy.y = mousePos.y
   })
 
-  globalShortcut.register('X', () => {
-    if(win.getOpacity() <= 0) {
-      win.setOpacity(0.9)
-    } else win.setOpacity(0)
-  })
   globalShortcut.register('B', () => {
     win.webContents.send('key', 'B', getxy())
   })
-  globalShortcut.register('N', () => {
-    win.webContents.send('key', 'N', getxy())
+    globalShortcut.register('N', () => {
+      win.webContents.send('key', 'N', getxy())
   })
-  globalShortcut.register('V', () => {
-    win.webContents.send('key', 'V', getxy())
+    globalShortcut.register('V', () => {
+      win.webContents.send('key', 'V', getxy())
   })
-  globalShortcut.register('F', () => {
-    win.webContents.send('key', 'F', getxy())
+    globalShortcut.register('F', () => {
+      win.webContents.send('key', 'F', getxy())
   })
-  globalShortcut.register('C', () => {
-    win.webContents.send('key', 'C', getxy())
+    globalShortcut.register('C', () => {
+      win.webContents.send('key', 'C', getxy())
   })
-  globalShortcut.register('M', () => {
-    win.webContents.send('key', 'M', getxy())
+    globalShortcut.register('M', () => {
+      win.webContents.send('key', 'M', getxy())
   })
-  globalShortcut.register('K', () => {
-    win.webContents.send('key', 'K', getxy())
+    globalShortcut.register('K', () => {
+      win.webContents.send('key', 'K', getxy())
   })
-  globalShortcut.register('Up', () => {
-    win.webContents.send('key', 'UP', getxy())
+    globalShortcut.register('Up', () => {
+      win.webContents.send('key', 'UP', getxy())
   })
-  globalShortcut.register('Down', () => {
-    win.webContents.send('key', 'DOWN', getxy())
+    globalShortcut.register('Down', () => {
+      win.webContents.send('key', 'DOWN', getxy())
   })
-})
+}
 
 ipcMain.on("synchronous-message", (event, arg) => {
 
   if(typeof arg === "string") {
     if(arg == "fullscreen") {
       win.setFullScreen(true);
+      location = 'bot';
+      register();
+      registerX();
     } else if(arg === "mouseOn") {
       win.setIgnoreMouseEvents(false);
     } else if(arg === "mouseOff") {
@@ -108,6 +123,7 @@ ipcMain.on("synchronous-message", (event, arg) => {
     if(arg.info === "reSize") {
       win.setSize(arg.width, arg.height);
       location = arg.location;
+      if(location == 'bot') register();
     } else if(arg.info === "ad") {
       shell.openExternal(arg.link);
       win.minimize();
